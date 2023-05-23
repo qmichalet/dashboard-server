3a5462f9361d3b79262af333edb07908aec56bcb
Quentin <33957011+qmichalet@users.noreply.github.com>
2023-05-23 17:47:06 +0200

    Feature : key performance indicators route (#1)
    * create kpis data models
    
    * created kpis route
    
    * injected mock data (1 time only)
    
    ---------
    
    Co-authored-by: Quentin Michalet <qmichalet@mini-de-quentin.home>

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+// import KPI from "./models/KPI.js";
+// import { kpis } from "./data/data.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -17,6 +20,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/* ROUTES */
+app.use("/kpi", kpiRoutes);
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
@@ -26,5 +32,9 @@ mongoose
 	})
 	.then(async () => {
 		app.listen(PORT, () => console.log(`Server Post: ${PORT}`));
+
+		// One time only data injection
+		// await mongoose.connection.db.dropDatabase();
+		// KPI.insertMany(kpis)
 	})
 	.catch((error) => console.log(`${error}, did not connect`));
