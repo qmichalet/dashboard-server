a119ae97994599d790ae2e6a4e275f983c76e771
Quentin Michalet <qmichalet@mini-de-quentin.home>
2023-05-25 11:19:53 +0200

    new product route + new data injection (1 time)

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+// import Product from "./models/Product.js";
 // import KPI from "./models/KPI.js";
-// import { kpis } from "./data/data.js";
+// import { kpis, products } from "./data/data.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -22,6 +24,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -35,6 +38,7 @@ mongoose
 
 		// One time only data injection
 		// await mongoose.connection.db.dropDatabase();
-		// KPI.insertMany(kpis)
+		// KPI.insertMany(kpis);
+		// Product.insertMany(products);
 	})
 	.catch((error) => console.log(`${error}, did not connect`));
